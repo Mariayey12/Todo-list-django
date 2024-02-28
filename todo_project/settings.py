e8e98817d27f0d38f3b6c3024442d7793805abb2
Mariayey12 <mmariayennifer.25@gmail.com>
2024-02-28 13:28:45 -0500

    Actualizaciones

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9$8qo73j-f6&&d1z*p9adkz6us3925w-f0dt_88kmac4&*0vix'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
